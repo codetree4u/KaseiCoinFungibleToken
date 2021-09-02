8438bb5ec67158a1f21aa1898f7df413467d0715
reachcarlos <80983254+reachcarlos@users.noreply.github.com>
2021-09-02 04:20:53 -0700

    Update KaseiCoin.sol

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -8,18 +8,16 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract KaseiCoin is ERC20, ERC20Detailed {
-    uint8 public constant DECIMALS = 18;
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
-
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor (
-        string memory name, 
-        string memory symbol
-        
-        ) public ERC20Detailed(name,symbol, DECIMALS) {
-        _mint(msg.sender, INITIAL_SUPPLY);
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    
+    
+// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+    constructor(
+    string memory name, 
+    string memory symbol, 
+    uint initial_suppy
+    ) ERC20Detailed(name, symbol, 18)  public {
+        mint(msg.sender, initial_suppy);
     }
+
 }
